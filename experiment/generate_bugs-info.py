b97af038eb1f056f50c7d8fef558eada36a8c7fd
jerryyangboyu <yangboyu@yangboyu.net>
2023-10-28 18:47:33 +0100

    add 30-in-395 bugs info

--- experiment/generate_bugs-info.py
+++ experiment/generate_bugs-info.py
@@ -6,12 +6,12 @@ from typing import Dict, List
 
 def get_value(content: str) -> str:
     value = content.split("=")[1]
-    return value[1:-1]
+    return value[1:-2]
 
 
 def extract_bugs_info(project_path: str, bug_id: int, project_link: str) -> Dict:
     with open(os.path.join(project_path, "bugs", str(bug_id), "bug.info")) as f:
-        content = f.readline()
+        content = f.readlines()
         buggy_commit = get_value(content[1])
         fix_commit = get_value(content[2])
         test_file = get_value(content[3])
@@ -20,20 +20,21 @@ def extract_bugs_info(project_path: str, bug_id: int, project_link: str) -> Dict
             "id": bug_id,
             "fix_commit_link": project_link + "/commit/" + fix_commit,
             "buggy_commit_link": project_link + "/commit/" + buggy_commit,
-            "test_file": test_file
+            "test_file": test_file,
         }
 
     return bug_info
 
 
 def write_bugs_info(write_path: str, bugs_info: Dict):
-    with open(write_path) as f:
-        json.dump(bugs_info, f)
+    with open(write_path, "w") as f:
+        json.dump(bugs_info, f, indent=4)
 
 
 def extract_project_github_link(project_path: str) -> str:
     with open(os.path.join(project_path, "project.info")) as f:
-        return get_value(f.readline()[0])
+        lines = f.readlines()
+        return get_value(lines[0])
 
 
 def match_project_dir(project_path_list: List[str], project_name: str) -> str:
@@ -44,31 +45,45 @@ def match_project_dir(project_path_list: List[str], project_name: str) -> str:
 
 def main():
     parser = argparse.ArgumentParser(description="Create prompt and write result")
-    parser.add_argument('-d', '--database', help="Database name, should be a folder name under the database directory",
-                        type=str, required=True)
+    parser.add_argument(
+        "-d",
+        "--database",
+        help="Database name, should be a folder name under the database directory",
+        type=str,
+        required=True,
+    )
 
     args = parser.parse_args()
 
     database = args.database
-    bugs_data: Dict = json.load(open(os.path.join("..", "database", "subsets-list", database), "r", encoding="utf-8"))
+    bugs_data: Dict = json.load(
+        open(
+            os.path.join("..", "database", "subsets-list", f"{database}.json"),
+            "r",
+            encoding="utf-8",
+        )
+    )
 
-    bgp_path = os.path.join("..", "benchmarks", "PyRepair", "benchmarks", "BugsInPy", "projects")
+    bgp_path = os.path.join(
+        "..", "benchmarks", "PyRepair", "benchmarks", "BugsInPy", "projects"
+    )
     project_path_list = [f.path for f in os.scandir(bgp_path) if f.is_dir()]
+
+    bugs_info_path = os.path.join("..", "database", database, "bugs-info")
+    os.makedirs(bugs_info_path, exist_ok=True)
+
     for project in bugs_data.keys():
         project_path = match_project_dir(project_path_list, project)
         project_link = extract_project_github_link(project_path)
 
-        bugs_info = {
-            "project": project,
-            "project_link": project_link,
-            "bugs": []
-        }
+        bugs_info = {"project": project, "project_link": project_link, "bugs": []}
 
-        for bug_id in project:
-            bugs_info["bugs"].append(extract_bugs_info(project_path, bug_id, project_link))
+        for bug_id in bugs_data[project]:
+            bugs_info["bugs"].append(
+                extract_bugs_info(project_path, bug_id, project_link)
+            )
 
-        bugs_info_path = os.path.join("..", "database", database, "bugs-info", project + ".json")
-        write_bugs_info(bugs_info_path, bugs_info)
+        write_bugs_info(os.path.join(bugs_info_path, f"{project}.json"), bugs_info)
 
 
 if __name__ == "__main__":
