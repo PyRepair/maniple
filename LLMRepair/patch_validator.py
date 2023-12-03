889eeda7723d5bb7d7d9390dbb8460f0d27fb803
jerryyangboyu <yangboyu@yangboyu.net>
2023-12-03 17:37:28 +0000

    improve patch validator

--- LLMRepair/patch_validator.py
+++ LLMRepair/patch_validator.py
@@ -1,14 +1,10 @@
+import json
 import os
 import subprocess
 from utils import print_in_red
 
 
-def validate_patches(bugid: str, output_dir: str):
-    full_bugdir_path = os.path.join(output_dir, "-".join(bugid.split(":")))
-    if not os.path.exists(full_bugdir_path):
-        print_in_red(f"ERROR: {full_bugdir_path} does not exist")
-        return
-    
+def validate_patches(bugid: str, bwd: str):
     # 0 pass test
     # 1 failed test
     # 2 invalid fix patch input for run custom patch (also include crash)
@@ -17,26 +13,29 @@ def validate_patches(bugid: str, output_dir: str):
 
     # subprocess.run(["bgp", "prep", "--bugids", bugid], check=True)
 
-    for filename in os.listdir(full_bugdir_path):
+    for filename in os.listdir(bwd):
         is_patchfile = "response" in filename and filename.endswith(".json")
         if not is_patchfile:
             continue
 
-        patchfile_path = os.path.join(full_bugdir_path, filename)
-        result_file_path = os.path.join(
-            full_bugdir_path, filename.replace("response", "result")
-        )
+        patchfile_path = os.path.join(bwd, filename)
+        result_file_path = os.path.join(bwd, filename.replace("response", "result"))
         error_log_path = os.path.join(
-            full_bugdir_path,
+            bwd,
             filename.replace("response", "log").replace(".json", ".txt"),
         )
 
         # subprocess.run(["bgp", "checkout_buggy", "--bugids", bugid], check=True)
-        output = subprocess.run(
-            ["run_custom_patch", patchfile_path, "--output-file", result_file_path],
-            check=True,
-            capture_output=True,
-        )
-        if output.stderr:
+
+        try:
+            subprocess.run(
+                ["run_custom_patch", patchfile_path, "--output-file", result_file_path],
+                check=True,
+                capture_output=True,
+            )
+        except subprocess.CalledProcessError as e:
+            msg = e.stderr.decode("utf-8") + "\n" + e.stdout.decode("utf-8")
             with open(os.path.join(error_log_path), "w") as f:
-                f.write(output.stderr.decode("utf-8"))
+                f.write(msg)
+            with open(result_file_path, "w") as f:
+                json.dump({bugid: 2}, f, indent=4)
