4571b2e053cd783eded26440675f7f25d998592f
jerryyangboyu <yangboyu@yangboyu.net>
2023-12-07 18:01:01 +0000

    patch validator support docker and external storage

--- LLMRepair/patch_validator.py
+++ LLMRepair/patch_validator.py
@@ -1,7 +1,29 @@
 import json
 import os
 import subprocess
-from utils import print_in_red, print_in_yellow, run_command
+from utils import print_in_yellow
+
+
+def is_patch_file_ok(patchfile_path: str, result_file_path: str, bugid: str) -> bool:
+    with open(patchfile_path) as f:
+        patchfile = json.load(f)
+
+    project_name = bugid.split(":")[0]
+
+    replace_code = patchfile[project_name][0]["replace_code"]
+    if replace_code is None:
+        print_in_yellow(f"Patch {patchfile_path} is None, skip...")
+        with open(result_file_path, "w") as f:
+            json.dump({bugid: 6}, f, indent=4)
+        return False
+
+    if replace_code == "":
+        print_in_yellow(f"Patch {patchfile_path} is empty, skip...")
+        with open(result_file_path, "w") as f:
+            json.dump({bugid: 7}, f, indent=4)
+        return False
+
+    return True
 
 
 def validate_patches(bugid: str, bwd: str, flag_overwrite: bool = False):
@@ -18,13 +40,7 @@ def validate_patches(bugid: str, bwd: str, flag_overwrite: bool = False):
     code 8: Run custom patch exception (bug from Nikhil, see error log)
     """
 
-    if flag_overwrite:
-        try:
-            print_in_yellow(f"Preparing {bugid} before submitting patches...")
-            run_command(["bgp", "prep", "--bugids", bugid, "--reinstall"], check=True)
-        except subprocess.CalledProcessError as e:
-            print_in_red(e.stderr.decode("utf-8"))
-            return
+    # assume bug has already been prepped
 
     for filename in os.listdir(bwd):
         is_patchfile = "response" in filename and filename.endswith(".json")
@@ -36,33 +52,28 @@ def validate_patches(bugid: str, bwd: str, flag_overwrite: bool = False):
             continue
 
         result_file_path = os.path.join(bwd, filename.replace("response", "result"))
-        with open(patchfile_path) as f:
-            patchfile = json.load(f)
-
-        project_name = bugid.split(":")[0]
-        replace_code = patchfile[project_name][0]["replace_code"]
-        if replace_code is None:
-            print_in_yellow(f"Patch {patchfile_path} is None, skip...")
-            with open(result_file_path, "w") as f:
-                json.dump({bugid: 6}, f, indent=4)
+        if not is_patch_file_ok(patchfile_path, result_file_path, bugid):
             continue
-
-        if replace_code == "":
-            print_in_yellow(f"Patch {patchfile_path} is empty, skip...")
-            with open(result_file_path, "w") as f:
-                json.dump({bugid: 7}, f, indent=4)
-            continue
-
-        log_file_name = filename.replace("response", "log").replace(".json", ".txt")
-        error_log_path = os.path.join(bwd, log_file_name)
 
         try:
-            run_command(
-                ["run_custom_patch", patchfile_path, "--output-file", result_file_path],
+            commands = (
+                f"docker run --rm -it "
+                + f"-v /Volumes/SSD2T/envs:/envs pyr:lite "
+                + f"-v {os.path.dirname(result_file_path)}:/RUN_CUSTOM_PATCH_DIR "
+                + f"run_custom_patch /RUN_CUSTOM_PATCH_DIR/{os.path.basename(patchfile_path)} "
+                + f"--output-file /RUN_CUSTOM_PATCH_DIR{os.path.basename(result_file_path)} "
+                + f"--separate-envs --envs-dir /envs"
+            )
+            commands = commands.split(" ")
+            subprocess.run(
+                commands,
                 check=True,
                 capture_output=True,
             )
+
         except subprocess.CalledProcessError as e:
+            log_file_name = filename.replace("response", "log").replace(".json", ".txt")
+            error_log_path = os.path.join(bwd, log_file_name)
             msg = e.stderr.decode("utf-8") + "\n" + e.stdout.decode("utf-8")
             with open(os.path.join(error_log_path), "w") as f:
                 f.write(msg)
