9946b7d07ce3350fc3c89fcdff978751d11952dc
jerryyangboyu <yangboyu@yangboyu.net>
2023-12-11 16:56:25 +0000

    ignore keras:26 due to tensorflow issues

--- LLMRepair/utils.py
+++ LLMRepair/utils.py
@@ -6,7 +6,10 @@ import re
 from typing import Dict, Optional
 
 
-IGNORED_BUGS = ["spacy:2", "spacy:3", "spacy:5", "scrapy:11"]
+IGNORED_BUGS = [
+    "spacy:2", "spacy:3", "spacy:5", "scrapy:11", # 106
+                "keras:26" # 395
+                ]
 
 
 class NotSupportedError(Exception):
