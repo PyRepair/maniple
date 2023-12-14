40d6b7b4e7b265dbd675e29946eab28820e7d9f7
HuijieYan <Huijie.Yan@Outlook.com>
2023-12-14 18:30:20 +0000

    update result sheet+

--- LLMRepair/generate_result_sheet.py
+++ LLMRepair/generate_result_sheet.py
@@ -2,117 +2,120 @@ import json
 import os
 import pandas as pd
 
-dataset_name = "395-dataset"
-database_path = os.path.join("..", "training-data", dataset_name, "bugs-data")
-result_sheet_folder = os.path.join("..", "training-data", "result-sheet", dataset_name)
+dataset = ["395-dataset", "106-dataset"]
 
-if not os.path.exists(result_sheet_folder):
-    os.makedirs(result_sheet_folder)
+for dataset_name in dataset:
 
-use_strata = 1
-only_available_fact_strata = 1
+    database_path = os.path.join("..", "training-data", dataset_name, "bugs-data")
+    result_sheet_folder = os.path.join("..", "training-data", "result-sheet", dataset_name)
 
-if use_strata == 1:
-    data = {
-        "Project": [],
-        "Bug_id": [],
-        "Strata 1:": [],
-        "Strata 2": [],
-        "Strata 3": [],
-        "Strata 4": [],
-        "Strata 5": [],
-        "Strata 6": [],
-        "Strata 7": [],
-        "Pass Test": [],
-        "Trial": [],
-        "result_filename": []
-    }
-else:
-    data = {
-        "Project": [],
-        "Bug_id": [],
-        "1.1.1 Buggy function code": [],
-        "1.1.2 buggy function docstring": [],
-        "1.2.1 Buggy class declaration": [],
-        "1.2.2 buggy class docstring": [],
-        "1.2.3 Buggy class scope invoked method signature": [],
-        "1.3.1 Buggy file name": [],
-        "1.3.2 Buggy file scope invoked function signature": [],
-        "1.4.1 Test code": [],
-        "1.4.2 Test file name": [],
-        "2.1.1 Error message": [],
-        "2.1.2 Error stack trace": [],
-        "2.1.3 Variable runtime value": [],
-        "2.1.4 Variable runtime type": [],
-        "2.1.5 Angelic value": [],
-        "2.1.6 Angelic type": [],
-        "3.1.1 Github linked issue title": [],
-        "3.1.2 Github linked issue description": [],
-        "cot Chain of Thought prompt technique": [],
-        "Pass Test": [],
-        "Trial": [],
-        "result_filename": []
-    }
+    if not os.path.exists(result_sheet_folder):
+        os.makedirs(result_sheet_folder)
 
-count = 0
-for project_name in os.listdir(database_path):
-    project_path = os.path.join(database_path, project_name)
-    for bug_id in os.listdir(project_path):
-        bug_path = os.path.join(project_path, bug_id)
-        for result_file_name in os.listdir(bug_path):
-            if "result" not in result_file_name:
-                continue
+    use_strata = 1
+    only_available_fact_strata = 1
 
-            with open(os.path.join(bug_path, result_file_name), "r") as result_file:
-                result = json.load(result_file)
-                pass_test = result[f"{project_name}:{bug_id}"]
+    if use_strata == 1:
+        data = {
+            "Project": [],
+            "Bug_id": [],
+            "Strata 1:": [],
+            "Strata 2": [],
+            "Strata 3": [],
+            "Strata 4": [],
+            "Strata 5": [],
+            "Strata 6": [],
+            "Strata 7": [],
+            "Pass Test": [],
+            "Trial": [],
+            "result_filename": []
+        }
+    else:
+        data = {
+            "Project": [],
+            "Bug_id": [],
+            "1.1.1 Buggy function code": [],
+            "1.1.2 buggy function docstring": [],
+            "1.2.1 Buggy class declaration": [],
+            "1.2.2 buggy class docstring": [],
+            "1.2.3 Buggy class scope invoked method signature": [],
+            "1.3.1 Buggy file name": [],
+            "1.3.2 Buggy file scope invoked function signature": [],
+            "1.4.1 Test code": [],
+            "1.4.2 Test file name": [],
+            "2.1.1 Error message": [],
+            "2.1.2 Error stack trace": [],
+            "2.1.3 Variable runtime value": [],
+            "2.1.4 Variable runtime type": [],
+            "2.1.5 Angelic value": [],
+            "2.1.6 Angelic type": [],
+            "3.1.1 Github linked issue title": [],
+            "3.1.2 Github linked issue description": [],
+            "cot Chain of Thought prompt technique": [],
+            "Pass Test": [],
+            "Trial": [],
+            "result_filename": []
+        }
 
-            with open(os.path.join(bug_path, result_file_name.replace("result", "response")), "r") as response_file:
-                response = json.load(response_file)
-                if only_available_fact_strata == 0:
-                    fact_bitvector: dict = response[project_name][0]["bitvector"]
-                    strata_bitvector: dict = response[project_name][0]["strata"]
+    count = 0
+    for project_name in os.listdir(database_path):
+        project_path = os.path.join(database_path, project_name)
+        for bug_id in os.listdir(project_path):
+            bug_path = os.path.join(project_path, bug_id)
+            for result_file_name in os.listdir(bug_path):
+                if "result" not in result_file_name:
+                    continue
+
+                with open(os.path.join(bug_path, result_file_name), "r") as result_file:
+                    result = json.load(result_file)
+                    pass_test = result[f"{project_name}:{bug_id}"]
+
+                with open(os.path.join(bug_path, result_file_name.replace("result", "response")), "r") as response_file:
+                    response = json.load(response_file)
+                    if only_available_fact_strata == 0:
+                        fact_bitvector: dict = response[project_name][0]["bitvector"]
+                        strata_bitvector: dict = response[project_name][0]["strata"]
+                    else:
+                        fact_bitvector: dict = response[project_name][0]["available_bitvector"]
+                        strata_bitvector: dict = response[project_name][0]["available_strata"]
+
+                data["Project"].append(project_name)
+                data["Bug_id"].append(bug_id)
+
+                if use_strata == 1:
+                    data["Strata 1:"].append(strata_bitvector["1"])
+                    data["Strata 2"].append(strata_bitvector["2"])
+                    data["Strata 3"].append(strata_bitvector["3"])
+                    data["Strata 4"].append(strata_bitvector["4"])
+                    data["Strata 5"].append(strata_bitvector["5"])
+                    data["Strata 6"].append(strata_bitvector["6"])
+                    data["Strata 7"].append(strata_bitvector["7"])
                 else:
-                    fact_bitvector: dict = response[project_name][0]["available_bitvector"]
-                    strata_bitvector: dict = response[project_name][0]["available_strata"]
+                    data["1.1.1 Buggy function code"].append(fact_bitvector["1.1.1"])
+                    data["1.1.2 buggy function docstring"].append(fact_bitvector["1.1.2"])
+                    data["1.2.1 Buggy class declaration"].append(fact_bitvector["1.2.1"])
+                    data["1.2.2 buggy class docstring"].append(fact_bitvector["1.2.2"])
+                    data["1.2.3 Buggy class scope invoked method signature"].append(fact_bitvector["1.2.3"])
+                    data["1.3.1 Buggy file name"].append(fact_bitvector["1.3.1"])
+                    data["1.3.2 Buggy file scope invoked function signature"].append(fact_bitvector["1.3.2"])
+                    data["1.4.1 Test code"].append(fact_bitvector["1.4.1"])
+                    data["1.4.2 Test file name"].append(fact_bitvector["1.4.2"])
+                    data["2.1.1 Error message"].append(fact_bitvector["2.1.1"])
+                    data["2.1.2 Error stack trace"].append(fact_bitvector["2.1.2"])
+                    data["2.1.3 Variable runtime value"].append(fact_bitvector["2.1.3"])
+                    data["2.1.4 Variable runtime type"].append(fact_bitvector["2.1.4"])
+                    data["2.1.5 Angelic value"].append(fact_bitvector["2.1.5"])
+                    data["2.1.6 Angelic type"].append(fact_bitvector["2.1.6"])
+                    data["3.1.1 Github linked issue title"].append(fact_bitvector["3.1.1"])
+                    data["3.1.2 Github linked issue description"].append(fact_bitvector["3.1.2"])
+                    data["cot Chain of Thought prompt technique"].append(fact_bitvector["cot"])
 
-            data["Project"].append(project_name)
-            data["Bug_id"].append(bug_id)
+                data["Pass Test"].append(pass_test)
+                data["Trial"].append(int(result_file_name[-6]))
+                data["result_filename"].append(result_file_name)
 
-            if use_strata == 1:
-                data["Strata 1:"].append(strata_bitvector["1"])
-                data["Strata 2"].append(strata_bitvector["2"])
-                data["Strata 3"].append(strata_bitvector["3"])
-                data["Strata 4"].append(strata_bitvector["4"])
-                data["Strata 5"].append(strata_bitvector["5"])
-                data["Strata 6"].append(strata_bitvector["6"])
-                data["Strata 7"].append(strata_bitvector["7"])
-            else:
-                data["1.1.1 Buggy function code"].append(fact_bitvector["1.1.1"])
-                data["1.1.2 buggy function docstring"].append(fact_bitvector["1.1.2"])
-                data["1.2.1 Buggy class declaration"].append(fact_bitvector["1.2.1"])
-                data["1.2.2 buggy class docstring"].append(fact_bitvector["1.2.2"])
-                data["1.2.3 Buggy class scope invoked method signature"].append(fact_bitvector["1.2.3"])
-                data["1.3.1 Buggy file name"].append(fact_bitvector["1.3.1"])
-                data["1.3.2 Buggy file scope invoked function signature"].append(fact_bitvector["1.3.2"])
-                data["1.4.1 Test code"].append(fact_bitvector["1.4.1"])
-                data["1.4.2 Test file name"].append(fact_bitvector["1.4.2"])
-                data["2.1.1 Error message"].append(fact_bitvector["2.1.1"])
-                data["2.1.2 Error stack trace"].append(fact_bitvector["2.1.2"])
-                data["2.1.3 Variable runtime value"].append(fact_bitvector["2.1.3"])
-                data["2.1.4 Variable runtime type"].append(fact_bitvector["2.1.4"])
-                data["2.1.5 Angelic value"].append(fact_bitvector["2.1.5"])
-                data["2.1.6 Angelic type"].append(fact_bitvector["2.1.6"])
-                data["3.1.1 Github linked issue title"].append(fact_bitvector["3.1.1"])
-                data["3.1.2 Github linked issue description"].append(fact_bitvector["3.1.2"])
-                data["cot Chain of Thought prompt technique"].append(fact_bitvector["cot"])
-
-            data["Pass Test"].append(pass_test)
-            data["Trial"].append(int(result_file_name[-6]))
-            data["result_filename"].append(result_file_name)
-
-df = pd.DataFrame(data)
-if use_strata == 1:
-    df.to_excel(os.path.join(result_sheet_folder, "raw_result_strata.xlsx"), index=False)
-else:
-    df.to_excel(os.path.join(result_sheet_folder, "raw_result_fact.xlsx"), index=False)
+    df = pd.DataFrame(data)
+    if use_strata == 1:
+        df.to_excel(os.path.join(result_sheet_folder, "raw_result_strata.xlsx"), index=False)
+    else:
+        df.to_excel(os.path.join(result_sheet_folder, "raw_result_fact.xlsx"), index=False)
--- LLMRepair/plot_result_chart.py
+++ LLMRepair/plot_result_chart.py
@@ -11,23 +11,23 @@ from upsetplot import plot
 
 
 def create_raw_bitvector_fix_rate(test_data, filename):
-    test_data['bitvector'] = test_data.iloc[:, 2:19].apply(lambda row: ''.join(row.values.astype(str)), axis=1)
+    test_data['bitvector'] = test_data.iloc[:, 2:9].apply(lambda row: ''.join(row.values.astype(str)), axis=1)
     # Group by the 'bitvector' and count the values in column 19
-    grouped_by_bitvector = test_data.groupby(['bitvector', test_data.columns[19]]).size().unstack(fill_value=0)
+    grouped_by_bitvector = test_data.groupby(['bitvector', test_data.columns[9]]).size().unstack(fill_value=0)
     # Reset index to make 'bitvector' a column
     bitvector_raw_fix_rate = grouped_by_bitvector.reset_index()
     bitvector_raw_fix_rate.to_excel(os.path.join(result_sheet_folder, filename), index=False)
 
 
 def create_raw_aggregate_fix_rate(test_data, filename):
-    test_result_codes = test_data.iloc[:, 19].value_counts()
+    test_result_codes = test_data.iloc[:, 9].value_counts()
     aggregate_raw_fix_rate = pd.DataFrame(test_result_codes).transpose()
     aggregate_raw_fix_rate.to_excel(os.path.join(result_sheet_folder, filename), index=False)
 
 
 def create_fix_probability(test_data, filename):
-    group_columns = test_data.columns[:19].tolist()
-    result_column = test_data.columns[19]
+    group_columns = test_data.columns[:9].tolist()
+    result_column = test_data.columns[9]
     # Group by the specified columns
     grouped = test_data.groupby(group_columns)
     # Calculate the new columns
@@ -49,29 +49,38 @@ def create_fix_rate_for_each_bug(fix_rate_data, filename):
 
 def create_bitvector_fix_rate(fix_rate_data, filename):
     # Display the resulting dataframe
-    fix_rate_for_bitvector = fix_rate_data.groupby(fix_rate_data.columns[2:19].tolist()).agg(
+    fix_rate_for_bitvector = fix_rate_data.groupby(fix_rate_data.columns[2:9].tolist()).agg(
         total_fix_patch=('fix_patch_count', 'sum'),
         total_response=('total_response_count', 'sum')
     ).reset_index()
-    fix_rate_for_bitvector['bitvector'] = fix_rate_for_bitvector[fix_rate_data.columns[2:19].tolist()].astype(
+    fix_rate_for_bitvector['bitvector'] = fix_rate_for_bitvector[fix_rate_data.columns[2:9].tolist()].astype(
         str).agg(''.join, axis=1)
     grouped_df = fix_rate_for_bitvector[['bitvector', 'total_response', 'total_fix_patch']]
     grouped_df.to_excel(os.path.join(result_sheet_folder, filename), index=False)
 
 
-dataset_name = "106-dataset"
+def keep_top_5_highest_fix_rate_bitvector(input_dict):
+    # Sort the input_dict based on the length of the value dict in descending order
+    sorted_dict = sorted(input_dict.items(), key=lambda x: len(x[1]), reverse=True)
+
+    # Keep only the top 10
+    top_10 = dict(sorted_dict[:5])
+
+    return top_10
+
+
+dataset_name = "395-dataset"
 database_path = os.path.join("..", "training-data", dataset_name)
 result_sheet_folder = os.path.join("..", "training-data", "result-sheet", dataset_name)
-result_path = os.path.join(result_sheet_folder, "raw_result_fact.xlsx")
+result_path = os.path.join(result_sheet_folder, "raw_result_strata.xlsx")
 
 raw_data = pd.read_excel(result_path, engine='openpyxl')
 
-
-create_raw_bitvector_fix_rate(raw_data, "raw_fix_rate")
-create_raw_aggregate_fix_rate(raw_data, "raw_aggregate_fix_rate")
-fix_rate = create_fix_probability(raw_data, "fix_probability")
-create_fix_rate_for_each_bug(fix_rate, "fix_rate_for_each_bug")
-create_bitvector_fix_rate(fix_rate, "fix_rate_for_bitvector")
+create_raw_bitvector_fix_rate(raw_data, "raw_strata_fix_rate.xlsx")
+create_raw_aggregate_fix_rate(raw_data, "raw_aggregate_fix_rate.xlsx")
+fix_rate = create_fix_probability(raw_data, "strata_fix_probability_for_each_bug.xlsx")
+create_fix_rate_for_each_bug(fix_rate, "fix_rate_for_each_bug.xlsx")
+create_bitvector_fix_rate(fix_rate, "strata_fix_rate.xlsx")
 
 
 fix_succeed = fix_rate[fix_rate['fix_probability'] > 0]
@@ -81,7 +90,7 @@ result_dict: dict = {}
 # Iterate through each row of the filtered DataFrame
 for index, row in fix_succeed.iterrows():
     # Concatenate the values of columns 3 to 19 as the code key
-    code_key = ''.join(row.iloc[2:19].astype(str))
+    code_key = ''.join(row.iloc[2:9].astype(str))
 
     # Create the project_bug_id string
     project_bug_id = f"{row['Project']}:{row['Bug_id']}"
@@ -93,50 +102,32 @@ for index, row in fix_succeed.iterrows():
         # If the code_key is already in the dictionary, add the new project_bug_id
         result_dict[code_key].add(project_bug_id)
 
+top_10_bitvector = keep_top_5_highest_fix_rate_bitvector(result_dict)
 
-upset_data: DataFrame = upsetplot.from_contents(result_dict)
+if "1111111" not in top_10_bitvector:
+    top_10_bitvector["1111111"] = result_dict["1111111"]
+if "1000000" not in top_10_bitvector:
+    top_10_bitvector["1000000"] = result_dict["1000000"]
+
+
+upset_data: DataFrame = upsetplot.from_contents(top_10_bitvector)
 
 # Create an UpSet plot
 ax = upsetplot.plot(upset_data, subset_size='count', show_counts='%d', sort_by="cardinality")
 
-# Calculate intersections
-all_keys = list(result_dict.keys())
-intersections = {}
-
-for i in range(1, len(result_dict) + 1):
-    for subset in combinations(all_keys, i):
-        common_elements = set.intersection(*[result_dict[key] for key in subset])
-        if common_elements:
-            intersections[subset] = common_elements
-
-# Print intersections and their elements
-for subset, elements in intersections.items():
-    print(f"Intersection {subset}: {len(elements)} elements - {', '.join(elements)}")
-
+plt.savefig(os.path.join(result_sheet_folder, "strata_fix_rate_upset.png"))
 plt.show()
 
 
-# df_list = []
-# for code, bugs in result_dict.items():
-#     for bug in bugs:
-#         row = {'bug_id': bug}
-#         for i, digit in enumerate(code):
-#             row[f'feature_{i+1}'] = int(digit)
-#         df_list.append(row)
+# all_keys = list(result_dict.keys())
+# intersections = {}
 #
-# df = pd.DataFrame(df_list)
-# df = df.set_index('bug_id')
-# df = df.groupby('bug_id').max()  # Assuming a bug_id should not repeat across different codes
+# for i in range(1, len(result_dict) + 1):
+#     for subset in combinations(all_keys, i):
+#         common_elements = set.intersection(*[result_dict[key] for key in subset])
+#         if common_elements:
+#             intersections[subset] = common_elements
 #
-# # Generating memberships
-# memberships = []
-# for index, row in df.iterrows():
-#     active_features = [f'feature_{i+1}' for i, digit in enumerate(row) if digit == 1]
-#     memberships.append(active_features)
-#
-# # Generating UpSet data
-# upset_data = upsetplot.from_memberships(memberships)
-#
-# # Plotting
-# ax = upsetplot.plot(upset_data, subset_size='count', show_counts='%d', sort_by="cardinality")
-# plt.show()
+# # Print intersections and their elements
+# for subset, elements in intersections.items():
+#     print(f"Intersection {subset}: {len(elements)} elements - {', '.join(elements)}")
