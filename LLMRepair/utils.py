eb76c392da39e96f702cd2c6a7d9693b7ed631bd
jerryyangboyu <yangboyu@yangboyu.net>
2023-12-18 15:07:01 +0000

    Refactor extract_function_from_code_block to extract_function_and_imports_from_code_block

--- LLMRepair/utils.py
+++ LLMRepair/utils.py
@@ -140,88 +140,16 @@ def find_patch_from_response(
     return None
 
 
-def extract_function_from_code_block(code_block: str, func_name: str) -> Optional[str]:
-    try:
-        return _extract_function_from_code_block_impl(code_block, func_name)
-    except Exception:
-        return None
-
-
-def _extract_function_from_code_block_impl(
-    code_block: str, func_name: str
-) -> Optional[str]:
-    """
-    code block: valid python source code
-    func_name: name of the function to be extracted
-    """
-
-    # Parsing the source code into an AST
-    tree = ast.parse(code_block)
-
-    # Find the function node and import statements
-    function_node = None
-    import_statements = []
-    for node in ast.walk(tree):
-        if isinstance(node, ast.FunctionDef) and node.name == func_name:
-            function_node = node
-        elif isinstance(node, (ast.Import, ast.ImportFrom)):
-            import_statements.append(node)
-
-    if not function_node:
-        return None
-
-    # Get the source code of the function's signature
-    start_line = function_node.lineno - 1
-    end_line = (
-        function_node.body[0].lineno - 2
-    )  # The line before the function body starts
-    function_signature = "\n".join(code_block.splitlines()[start_line : end_line + 1])
-
-    # Get the function body
-    function_body_lines = code_block.splitlines()[
-        function_node.body[0].lineno - 1 : function_node.end_lineno
-    ]
-
-    # Determine the minimum indentation in the function body
-    min_indent = min(
-        (
-            len(line) - len(line.lstrip())
-            for line in function_body_lines
-            if line.strip()
-        ),
-        default=0,
-    )
-
-    indent_spaces = " " * 4
-
-    # Adjust the indentation for the function body
-    function_body = "\n".join(
-        indent_spaces + line[min_indent:] for line in function_body_lines
-    )
-
-    # Extract import statements as source code with appropriate indentation
-    imports_code = "\n".join(
-        indent_spaces + ast.get_source_segment(code_block, node) for node in import_statements  # type: ignore
-    )  # type: ignore
-
-    # Combine the function signature, imports, and function body
-    modified_function = (
-        function_signature.lstrip() + "\n" + imports_code + "\n" + function_body
-    )
-
-    return modified_function
-
-
-def extract_function_and_tuple_from_code_block(
+def extract_function_and_imports_from_code_block(
     code_block: str, func_name: str
 ) -> Optional[Tuple[str, List[str]]]:
     try:
-        return _extract_function_and_tuple_from_code_block_impl(code_block, func_name)
+        return _extract_function_and_imports_from_code_block_impl(code_block, func_name)
     except Exception:
         return None
 
 
-def _extract_function_and_tuple_from_code_block_impl(
+def _extract_function_and_imports_from_code_block_impl(
     code_block: str, func_name: str
 ) -> Optional[Tuple[str, List[str]]]:
     """
