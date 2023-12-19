63a5cb13df26a50466e99afc7b6ef98023ecdb29
jerryyangboyu <yangboyu2021@outlook.com>
2023-12-19 22:59:18 +0000

    add examples

--- examples/run_all_106_validation.sh
+++ examples/run_all_106_validation.sh
@@ -1,5 +1,5 @@
 python3 LLMRepair validate \
-    --dataset 395subset  \
+    --dataset 106subset  \
     --envs-dir ~/Desktop/bgp_envs_local \
     --timeout 30 \
     --partitions 1 --overwrite --use-supported
