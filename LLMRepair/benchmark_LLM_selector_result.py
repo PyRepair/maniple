6337d67ae116283cf807f9ff08d85c82d9ad1404
jerryyangboyu <yangboyu@yangboyu.net>
2024-01-29 20:56:59 +0000

    update benchmark_LLM_selector_result.py code

--- LLMRepair/benchmark_LLM_selector_result.py
+++ LLMRepair/benchmark_LLM_selector_result.py
@@ -35,7 +35,10 @@ def main():
 
     success_patches_counter = 0
     success_bugfix_counter = 0
-    total_counter = 0
+    total_patches_counter = 0
+    total_bugs_counter = 0
+    pass_k_sum = [0] * 10
+    success_rate_sum = 0
 
     bug_dir = Path.cwd().parent / "training-data" / "16-16-dataset-llm-feature-selector"
     for bugid, project_folder, bugid_folder in iter_bugid_folders(bug_dir):
@@ -52,12 +55,20 @@ def main():
         if success_count > 0:
             success_bugfix_counter += 1
         success_patches_counter += success_count
-        total_counter += num_trials
+        total_patches_counter += num_trials
+        total_bugs_counter += 1
+
+        for k in range(1, 11):
+            pass_k_sum[k - 1] += pass_at_k(num_trials, success_count, k)
+
+        success_rate_sum += success_count / num_trials
+
+    print_in_yellow(f"total fix patches: {success_patches_counter} out of {total_patches_counter}")
+    print_in_yellow(f"total bugfix: {success_bugfix_counter} out of {total_bugs_counter}")
+    print_in_yellow(f"success rate: {success_rate_sum / total_bugs_counter}")
 
     for k in range(1, 11):
-        print_in_yellow(f"pass@{k}: {pass_at_k(total_counter, success_patches_counter, k)}")
-    print_in_yellow(f"total fix patches: {success_patches_counter}")
-    print_in_yellow(f"total bugfix: {success_bugfix_counter}")
+        print_in_yellow(f"pass@{k}: {pass_k_sum[k - 1] / total_bugs_counter}")
 
 
 if __name__ == '__main__':
