c0d307d41c7079b96145e1427f2f8387b4bd0727
jerryyangboyu <yangboyu@yangboyu.net>
2024-02-02 11:16:19 +0000

    create new files for LLM summarizer experiment

---
+++ LLMRepair/LLM_summarizer.py
@@ -0,0 +1,12 @@
+import tiktoken
+from pathlib import Path
+from gpt_utils import get_responses_from_prompt, QueryException
+from utils import print_in_red, print_in_yellow, iter_bugid_folders
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
