8c2840dc68bf8cbc337447cca42c04a06c793b8b
jerryyangboyu <yangboyu@yangboyu.net>
2024-02-29 11:50:30 +0000

    update external facts

--- a.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-from maniple.utils.misc import iter_bugid_folders
-
-for bugid, project_folder, bug_folder in iter_bugid_folders(Path("experiment-initialization-resources/bug-data")):
-    original_fact_json = bug_folder / "bug-data.json"
-    original_fact_json.rename(bug_folder / f"static-dynamic-facts.json")
